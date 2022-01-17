5e50097d3132cc6f7e507facb8ee8752773c5140
cemil <cemiw033@gmail.com>
2022-01-17 20:44:48 +0400

    delegates

---
+++ ConsoleApp6/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class DocProgram
+    {
+        virtual public  void OpenDoc()
+        {
+            Console.WriteLine("Doc opened");
+        }
+        virtual public void EditDoc()
+        {
+            Console.WriteLine("Can Edit in Pro and Expert versions");
+        }
+        virtual public void SaveDoc()
+        {
+            Console.WriteLine("Can Save in Pro and Expert versions");
+        }
+    }
+    public class ProDocProgram : DocProgram
+    {
+        public override void OpenDoc()
+        {
+            Console.WriteLine("Doc opened");
+        }
+        public override sealed void EditDoc()
+        {
+            Console.WriteLine("Document Edited");
+        }
+
+        public override void SaveDoc()
+        {
+            Console.WriteLine("Document Saved in doc format, for pdf format buy Expert packet");
+
+        }
+    }
+    public class ExpertDocProgram : ProDocProgram
+    {
+        public override void OpenDoc()
+        {
+            base.OpenDoc();
+        }
+
+        public override void SaveDoc()
+        {
+            Console.WriteLine("Doc saved in pdf format");
+        }
+        public void EditDoc()
+        {
+            Console.WriteLine("Doc edited");
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,36 @@
+﻿
+public delegate void delegats();
+namespace ConsoleApp6 {    
+    class Program {
+        static void Main(string[] args) {
+            delegats deleg;
+            DocProgram docs;
+            string? t = Console.ReadLine();
+
+            if (t == "basic")
+            {
+                docs = new DocProgram();
+                deleg = new(docs.OpenDoc);
+                deleg += docs.EditDoc;
+                deleg += docs.SaveDoc;
+                deleg.Invoke();
+            }
+            else if(t == "pro")
+            {
+                docs = new ProDocProgram();
+                deleg = new(docs.OpenDoc);
+                deleg += docs.EditDoc;
+                deleg += docs.SaveDoc;
+                deleg.Invoke();
+            }
+            else if (t == "expert")
+            {
+                docs = new ExpertDocProgram();
+                deleg = new(docs.OpenDoc);
+                deleg += docs.EditDoc;
+                deleg += docs.SaveDoc;
+                deleg.Invoke();
+            }
+        }
+    }
+}
